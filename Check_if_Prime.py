bfd898240c35e1cf927b94d3b6d94cd570b1f0f2
Debjit Pal <debjitpal5040@gmail.com>
2021-07-14 20:54:34 +0530

    small edit

--- Check_if_Prime.py
+++ Check_if_Prime.py
@@ -3,16 +3,16 @@ print("This is a program to check if a number is prime or not")
 N = int(input("Please Enter a Number : "))
 # 1 is neither a prime nor a composite number
 if N == 1:
-   print("1 is neither a prime nor a composite number")
+    print("1 is neither a prime nor a composite number")
 # 2 is the only even prime number
 elif N == 2:
     print("2 is a prime number") 
 # All odd primes are greater than 2
 elif N > 2:
-   # check if even
+    # check if even
     if N%2==0:
-       print(N,"is not a prime number")
-       print(str(N)+' = '+str(2)+' × '+str(N//2))
+        print(N,"is not a prime number")
+        print(str(N)+' = '+str(2)+' × '+str(N//2))
     # check for factors
     else:
         for i in range(3,int(N**0.5)+1,2):
