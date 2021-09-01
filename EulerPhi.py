b3a42a00e85234d5b144ec91fd35c0ac0bfb9010
Debjit Pal <debjitpal5040@gmail.com>
2021-09-01 18:18:52 +0530

    added more files

--- EulerPhi.py
+++ EulerPhi.py
@@ -7,10 +7,10 @@ def phi(n):
         if (n % p == 0):  
             while (n % p == 0): 
                 n = n // p 
-            result -= int(result / p);
+            result -= result // p
         p += 1  
     if (n > 1): 
-        result -= int(result / n);
+        result -= result // n
     return result
 n=int(input("Input the value of n : "))
 print("phi("+str(n)+")","=",phi(n))
